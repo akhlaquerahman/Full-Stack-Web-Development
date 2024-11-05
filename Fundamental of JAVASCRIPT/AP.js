6e765d2de9a9d17c10688a8eda734f9386051357
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-11-05 10:31:27 +0530

    AP

---
+++ Fundamental of JAVASCRIPT/AP.js
@@ -0,0 +1,16 @@
+// method 1
+
+let n=20
+for(i= 1; i<2*n-1; i+=2){
+    console.log(i)
+}
+
+// method 2
+
+// let Nth=20
+// let start=1
+
+// for(i=1; i<Nth; i++){
+//     start+=2
+//     console.log(start)
+// }
