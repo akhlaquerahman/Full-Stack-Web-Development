72f94a139d4c6667e2125002f65f56359effc26a
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-11-06 11:05:33 +0530

    Person Age

---
+++ JAVASCRIPT/Conditional Statement in Js/personAge.js
@@ -0,0 +1,19 @@
+
+let age=15
+
+
+if(age < 12){
+    console.log("Child:")
+}
+
+else if(12 <= age < 18 ){
+    console.log("Teenager:")
+}
+
+else if(18 <= age < 60){
+    console.log("Adult: ")
+}
+
+else if(age >= 60 ){
+    console.log(" Senior Citizen: ")
+}
