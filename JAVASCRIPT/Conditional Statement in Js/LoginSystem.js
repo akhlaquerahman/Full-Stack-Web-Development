cc2504e47e191b06b968c8945a6e1540a220bdd3
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-11-06 11:32:14 +0530

    Login System username and Password

---
+++ JAVASCRIPT/Conditional Statement in Js/LoginSystem.js
@@ -0,0 +1,11 @@
+let username = "akhlaque@123"
+
+let Password = "12345678"
+
+if(username=="akhlaque@123"  &&  Password == "12345678"){
+    console.log("Login Succesfully")
+}
+
+else{
+    console.log("incorrect username and Password")
+}
