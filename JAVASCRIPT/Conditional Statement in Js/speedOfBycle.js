fab543ce0b3a345bf1d5981d29be47819c38135d
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-11-06 11:21:22 +0530

    Speed Of Bycycle

---
+++ JAVASCRIPT/Conditional Statement in Js/speedOfBycle.js
@@ -0,0 +1,15 @@
+
+let speed=75
+
+
+if(speed <= 60){
+    console.log("Speed is Normal")
+}
+
+else if(speed > 61 && speed<=100 ){
+    console.log("Speed is High" )
+}
+
+else if(speed>100 ){
+    console.log( "Speed is Too High" )
+}
