2cd0ec6547c903ef11ad3c280381148d53580de5
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-11-07 09:47:39 +0530

    Print AP

--- JAVASCRIPT/Loops Statement/AP.js
+++ JAVASCRIPT/Loops Statement/AP.js
@@ -1,16 +1,16 @@
 // method 1
 
-let num=20
-for(i= 1; i<2*num-1; i+=2){
-    console.log(i)
-}
+// let num=20
+// for(i= 1; i<2*num-1; i+=2){
+//     console.log(i)
+// }
 
 // method 2
 
-// let Nth=20
-// let start=1
+let Nth=20
+let start=1
 
-// for(i=1; i<Nth; i++){
-//     start+=2
-//     console.log(start)
-// }
+for(i=1; i<Nth; i++){
+    start+=2
+    console.log(start)
+}
