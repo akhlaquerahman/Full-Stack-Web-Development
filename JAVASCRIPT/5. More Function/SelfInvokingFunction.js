55f4fc6384e6cda8f3143869e9f587a2c5dd12aa
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-11-19 10:33:50 +0530

    Self Invoking Function

--- JAVASCRIPT/5. More Function/SelfInvokingFunction.js
+++ JAVASCRIPT/5. More Function/SelfInvokingFunction.js
@@ -1,4 +1,4 @@
-(function dis () {   //SelfInvoking Function Without call automatic call
+(function dis () {          //SelfInvoking Function Without call automatic call
     console.log("hello");
 })()
                             //IIFF - Immediatly invoke Function expression
