2ee668a9351ee6ee64d40cce4fab31863fb55cdc
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-11-19 10:25:24 +0530

    Print Arrow Functio Square Return

--- JAVASCRIPT/5. More Function/ArrowFuntionSquareReturn.js
+++ JAVASCRIPT/5. More Function/ArrowFuntionSquareReturn.js
@@ -1,7 +1,7 @@
-// const square = (a) => {
-//     return a * a;
-// }
-// console.log(square(6))
+/*  const square = (a) => {
+    return a * a;
+}
+console.log(square(6))  */
 
 const square = a =>  a * a;   //Its Write One Liner
    
