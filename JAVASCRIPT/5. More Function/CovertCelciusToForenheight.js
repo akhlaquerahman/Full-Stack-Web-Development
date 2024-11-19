6f604f634373e89f1c9e70f7fff092bfe5c2583d
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-11-19 10:26:46 +0530

    Convert Celcius to forenheight using Arrow Function

--- JAVASCRIPT/5. More Function/CovertCelciusToForenheight.js
+++ JAVASCRIPT/5. More Function/CovertCelciusToForenheight.js
@@ -1,8 +1,8 @@
-// const celciusToForen = (c) => {
-//     return (c * 9 / 5) + 32;
-// }
+/*  const celciusToForen = (c) => {
+    return (c * 9 / 5) + 32;
+}
 
-// console.log(celciusToForen(25))
+console.log(celciusToForen(25))  */
 
 const celciusToForen = c => (c * 9 / 5) + 32;
 
