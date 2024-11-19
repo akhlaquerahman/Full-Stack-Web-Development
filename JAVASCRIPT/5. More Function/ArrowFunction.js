6d6c6f19af61f623ecd840ee92baccb8a2cd7ea5
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-11-19 10:22:25 +0530

    Print Annonymous Funtion

---
+++ JAVASCRIPT/5. More Function/ArrowFunction.js
@@ -0,0 +1,12 @@
+/*  const display=()=>{
+    console.log("Hello, World!");
+}
+
+display();          */
+
+
+const display = (a,b) => {
+    return a+b
+}
+console.log(display(4,5))
+
---
+++ JAVASCRIPT/5. More Function/EvenOrOdd.js
@@ -0,0 +1,13 @@
+const evenOrOdd = (a) => {
+    if(a % 2 === 0){
+        return "Even Number"
+}
+else
+{
+    return "Odd Number"
+    }
+
+}
+
+console.log(evenOrOdd(5))
+
