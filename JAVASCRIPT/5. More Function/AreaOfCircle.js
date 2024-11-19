9e72e640255ea759c6e1821c67235b1601280c8f
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-11-19 10:23:40 +0530

    Print Area of Circle using Arrow Funcion

--- JAVASCRIPT/5. More Function/AreaOfCircle.js
+++ JAVASCRIPT/5. More Function/AreaOfCircle.js
@@ -1,8 +1,8 @@
-// const area = (r) =>{
-//     return 3.14 * r * r;
-// }
+/* const area = (r) =>{
+    return 3.14 * r * r;
+}
 
-// console.log(area(14))
+console.log(area(14))  */
 
 
 const area = r => 3.14 * r * r
