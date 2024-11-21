d06fdfc8e9a2e55cbe590910a58b58d0ff270343
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-11-21 17:09:27 +0530

    Convert into Second

---
+++ JAVASCRIPT/4. Function of js/Assignment/ConvertHourIntoSecond.js
@@ -0,0 +1,11 @@
+function ConvertHourIntoSecond(){
+    let Hour = 2
+    let minnute = Hour * 60
+    let Second = minnute * 60
+
+    return Second
+}
+
+console.log(`The Second is in Hour = ${ConvertHourIntoSecond()}`)
+
+
