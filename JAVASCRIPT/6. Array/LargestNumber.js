985f33500935ef5fb037a57279483d3467e23d5d
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-11-24 15:10:02 +0530

    Find Largest Element in Array

---
+++ JAVASCRIPT/6. Array/LargestNumber.js
@@ -0,0 +1,8 @@
+const Largest = [10,12,45,35,62,70]
+let Largest2 = Largest[0]
+for(let i=0; i<Largest.length; i++){
+    if(Largest[i] > Largest2){
+        Largest2 = Largest[i]
+    }
+}
+console.log(Largest2)
